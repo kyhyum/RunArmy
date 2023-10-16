df72ccc0a89db93e5f7457d563735560b26c4cfa
JangbeomChoi <jangbeom0325@gmail.com>
2023-10-16 20:43:36 +0900

    [Add/Update] 에너미 추가, 코인 스크립트 수정

--- Assets/JangbeomScripts/Coin/Coin.cs
+++ Assets/JangbeomScripts/Coin/Coin.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public TMP_Text scoreText;
+    
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, 100f) * Time.deltaTime);//È¸Àü
