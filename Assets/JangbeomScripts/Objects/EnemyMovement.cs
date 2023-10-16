2fbb1f237afd219b7334f9f85def6edf81a569f2
JangbeomChoi <jangbeom0325@gmail.com>
2023-10-16 21:16:07 +0900

    [Add/Update] 적 애니메이션 수정 및 플레이어 추적 추가

---
+++ Assets/JangbeomScripts/Objects/EnemyMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform player;
+    private NavMeshAgent nav;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+}
