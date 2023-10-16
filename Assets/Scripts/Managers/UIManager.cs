8415fecb8b2cf399d90984dca2d03e0d68063220
kyhyum <dudrhkd0225@naver.com>
2023-10-16 20:55:24 +0900

    [Add] UIManager, UIPopup (-영현)

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new UIManager();
+            return _instance;
+        }
+    }
+
+    private List<UIPopup> popups = new List<UIPopup>();
+
+    public UIPopup ShowPopup(string popupname)
+    {
+        var obj = Resources.Load("Popups/" + popupname, typeof(GameObject)) as GameObject;
+        if (!obj)
+        {
+            Debug.LogWarning("Failed to ShowPopup" + popupname);
+            return null;
+        }
+        return ShowPopupWithPrefab(obj, popupname);
+    }
+
+    public T ShowPopup<T>() where T : UIPopup
+    {
+        return ShowPopup(typeof(T).Name) as T;
+    }
+
+    public UIPopup ShowPopupWithPrefab(GameObject prefab, string popupName)
+    {
+        var obj = Instantiate(prefab);
+        return ShowPopup(obj, popupName);
+    }
+
+    public UIPopup ShowPopup(GameObject obj, string popupname)
+    {
+        var popup = obj.GetComponent<UIPopup>();
+        popups.Insert(0, popup);
+        
+        obj.SetActive(true);
+        //obj.transform.SetParent(??, false);
+        return popup;
+    }
+}
