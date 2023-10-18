29bf6d0301e1ddd2caeab5576936a975d2822b9e
JangbeomChoi <jangbeom0325@gmail.com>
2023-10-18 12:18:20 +0900

    [Update] enemy스크립트 수정
    - ontriggerenter메서드를 oncollisionenter메서드로 수정

--- Assets/JangbeomScripts/Objects/Enemy.cs
+++ Assets/JangbeomScripts/Objects/Enemy.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -11,17 +10,16 @@ public class Enemy : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.CompareTag("Player"))
+        if (collision.collider.CompareTag("Player"))
         {
-            Rigidbody otherRb = other.GetComponent<Rigidbody>();
+            Rigidbody otherRb = collision.collider.GetComponent<Rigidbody>();
             if (otherRb != null)
             {
-               
                 otherRb.AddForce(Vector3.forward * 10, ForceMode.Impulse);
             }
-            if(PacmanGameManager.Instance.score >= 400)
+            if (PacmanGameManager.Instance.score >= 400)
             {
                 PacmanGameManager.Instance.Success();
             }
@@ -29,7 +27,28 @@ public class Enemy : MonoBehaviour
             {
                 PacmanGameManager.Instance.GameOver();
             }
-            
         }
     }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        Rigidbody otherRb = other.GetComponent<Rigidbody>();
+    //        if (otherRb != null)
+    //        {
+
+    //            otherRb.AddForce(Vector3.forward * 10, ForceMode.Impulse);
+    //        }
+    //        if(PacmanGameManager.Instance.score >= 400)
+    //        {
+    //            PacmanGameManager.Instance.Success();
+    //        }
+    //        else
+    //        {
+    //            PacmanGameManager.Instance.GameOver();
+    //        }
+
+    //    }
+    //}
 }
