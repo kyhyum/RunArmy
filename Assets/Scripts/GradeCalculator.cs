913d9ade88319526fc7e81c8891871e7e2740203
JuChan Jeong <jjc6252498@gmail.com>
2023-10-19 20:23:55 +0900

    [Update] 엔딩 팝업 추가 구현 중, [Add] 스토리모드 판별 추가

--- Assets/Scripts/GradeCalculator.cs
+++ Assets/Scripts/GradeCalculator.cs
@@ -9,14 +9,16 @@ public class GradeCalculator : MonoBehaviour
     [SerializeField] private string[] grades = new string[5];
     [SerializeField] private int[] golds = new int[5];
 
-    public string CalculateGrade(int score, out int gold)
+    public string CalculateGrade(int score, out int gold, bool reverse = false)
     {
         string grade = string.Empty;
         gold = 0;
 
         for (int i = 0; i < grades.Length; i++)
         {
-            if (data.ScoreCriteria[i] - score <= 0)
+            int subScore = reverse ? score - data.ScoreCriteria[i] : data.ScoreCriteria[i] - score;
+
+            if (subScore <= 0)
             {
                 grade = grades[i];
                 gold = golds[i];
